b6e381a3a60162df9d187ea948e3ad27c80648dc
Michael Lauritsen <i3028841@gmail.com>
2024-05-04 23:49:49 +0200

    fixed access_token issue

--- Program.cs
+++ Program.cs
@@ -12,6 +12,22 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         {
             builder.Configuration.Bind("AzureAd", options);
             options.TokenValidationParameters.NameClaimType = "name";
+
+            options.Events = new JwtBearerEvents
+            {
+                OnMessageReceived = context =>
+                {
+                    var accessToken = context.Request.Query["access_token"];
+
+                    var path = context.HttpContext.Request.Path;
+                    if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/hubs"))
+                    {
+                        context.Token = accessToken;
+                    }
+
+                    return Task.CompletedTask;
+                }
+            };
         }, options => { builder.Configuration.Bind("AzureAd", options); });
 
 builder.Services.AddCors(options =>
